232fe81144c18de7ec6b81acab4e16633e01df13
Murad491940 <84373217+Murad491940@users.noreply.github.com>
2023-06-16 21:07:22 +0400

    StringTask

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String myWord = new String();
+            myWord.Trim();
+            myWord.TrimStart();
+            myWord.TrimEnd();
+
+        }
+    }
+}
---
+++ ConsoleApp18/String.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    public class String
+    {
+        
+        public void Trim()
+        {
+            string word = "    Hello   ".Trim();
+            Console.WriteLine(word.Trim());
+
+        }
+        public void TrimStart()
+        {
+            string word = "    Hello".Trim();
+            Console.WriteLine(word.Trim());
+        }
+        public void TrimEnd()
+        {
+            string word = "Hello   ".Trim();
+            Console.WriteLine(word.Trim());
+        }
+    }
+}
